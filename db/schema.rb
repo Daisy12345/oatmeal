1b501c3df621afbd624d0b680f4cfc37bd370759
Daisy Gokulan <RANZA@Daisys-MacBook-Pro.local>
2015-11-17 12:30:21 +1100

    backbone folders

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151116234511) do
+ActiveRecord::Schema.define(version: 20151117001406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
