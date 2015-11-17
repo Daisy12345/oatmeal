0ea0f759c502e5ed0108e48b15515a77d574a58a
Andrew Bird <andrewadb@zoho.com>
2015-11-17 16:58:35 +1100

    added logic to models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,26 @@
 class Flight < ActiveRecord::Base
   belongs_to :plane
   has_many :reservations
+
+
+	def listSeats
+		return self.plane.makeSeats
+	end
+	def listAvailableSeats
+		list = listSeats
+		self.reservations.each do |k|
+   			list.delete(k.seat)
+		end
+		return list
+	end
+	def listReservedSeats
+		list = []
+		self.reservations.each do |k|
+   			list.push(k.seat)
+		end
+		return list
+	end
+	def availableSeatsNumber
+		listAvailableSeats.length
+	end
 end
--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,3 +1,17 @@
 class Plane < ActiveRecord::Base
 	has_many :flights
+
+	def makeSeats
+		seatList = []
+		alph = ("A".."Z").to_a
+		for i in 0..self.rows - 1
+			for k in 0..self.columns - 1
+				seatList << (i + 1).to_s + alph[k]
+			end
+		end
+		return seatList
+	end
+
+
+
 end
