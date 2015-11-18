758ede295629ceac7fb072a7c7d819d917fdd517
RANZA <daisyranikesavan@gmail.com>
2015-11-19 09:48:11 +1100

    planeview added

--- app/assets/javascripts/collections/planes.js
+++ app/assets/javascripts/collections/planes.js
@@ -6,5 +6,3 @@ oatmeal.Planes = Backbone.Collection.extend({
 });
 
 var plane = new oatmeal.Plane();
-var plane1 = new oatmeal.Plane({name: "Daisy", rows: 5, columns: 12});
-var plane2 = new oatmeal.Plane();
