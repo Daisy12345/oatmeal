e188b403a7c2f97e3cce8dee0a41def8b7a7aa1f
Andrew Bird <andrewadb@zoho.com>
2015-11-19 14:15:58 +1100

    planes saving to db

--- app/assets/javascripts/models/plane.js
+++ app/assets/javascripts/models/plane.js
@@ -15,6 +15,4 @@ oatmeal.Plane = Backbone.Model.extend({
 			console.log(event);
 		} );
 	}
-
-
 });
--- app/assets/javascripts/views/planeview.js
+++ app/assets/javascripts/views/planeview.js
@@ -3,12 +3,44 @@ var oatmeal = oatmeal || {};
 oatmeal.PlaneView = Backbone.View.extend({
 	el: '#plane-page',
 
-	initialize: function(collection) {
-	this.listenTo(collection, 'add', function(plane){
-		console.log("planeview init function");
-		var view = new oatmeal.PlaneListView({model: plane});
-		view.render();
-	});
 
+	events: {
+		'submit .plane-form': 'onSubmit',
+	},
+
+	initialize: function(collection) {
+		this.listenTo(collection, 'add', function(plane){
+			console.log("planeview init function");
+			// var view = new oatmeal.PlaneListView({model: plane});
+			// view.render();
+
+		});
+		this.model.bind('change:oatmeal', this.render, this);
+	},
+	onSubmit: function(event) {
+		// alert("onSubmit clicked");
+		// don't do the normal form submit behaviour
+		event.preventDefault();
+		// alert(this.$('input[name=rows]').val())
+		var planeInfo = ({
+            name: this.$('input[name=name]').val(),
+            rows: this.$('input[name=rows]').val(),
+            columns: this.$('input[name=columns]').val()
+        });
+        console.log(planeInfo);
+        var newPlane = new oatmeal.Plane;
+        newPlane.set(planeInfo);
+        console.log(newPlane);
+
+        oatmeal.planes.create(planeInfo);
+
+
+        // this.collection.create(planeInfo);
+
+
+		// var $input = $(event.target).find(this.textFieldSelector);
+		// var content = $input.val();
+ 		// $input.val('').focus();
+		// app.secrets.create({content: content});
 	}
 });
